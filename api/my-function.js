a2dbd7c6a5181f4e16b104bf5c426eed578f1ef4
Ranveer Singh Walia <108180407+ranveer76@users.noreply.github.com>
2024-05-21 19:33:00 +0530

    Add files via upload

---
+++ api/my-function.js
@@ -0,0 +1,8 @@
+// api/my-function.js
+
+// Define your serverless function handler
+module.exports = async (req, res) => {
+    // Your serverless function logic goes here
+    res.status(200).json({ message: 'Hello from my serverless function!' });
+  };
+  
